53d344bf40469941a876a9e034c3f84f308d7b40
jewelzj <61851489+jewelzj@users.noreply.github.com>
2021-03-09 22:23:38 -0800

    finalize

--- src/vegaDemo.js
+++ src/vegaDemo.js
@@ -8,6 +8,7 @@ var citySet = [];
 const options = {
   config: {
     // Vega-Lite default configuration
+
   },
   init: (view) => {
     // initialize tooltip handler
@@ -19,7 +20,8 @@ const options = {
     //   loader: vega.loader({
     //     baseURL: "",
     //   }),
-    renderer: "canvas",
+    renderer: "svg",
+    stroke: "transparent"
   },
 };
 
@@ -43,9 +45,9 @@ function drawBarVegaLite() {
     const click = vl.selectMulti().encodings('color');
     const brush = vl.selectInterval().encodings('x');
       
-    const scale = {
+    const brusha = {
         domain: ['Walkable Trips', 'Commute Trips', 'Outdoor Trips', 'Traveling Trips'],
-        range: ['#e7ba52', '#a7a7a7', '#aec7e8', '#1f77b4']
+        range: ['#e7ba52', '#a7a7a7', '#aec7e8', '#ff9af3']
       };
     
     const trip_2020 = vl.markBar({opacity: 0.8})
@@ -53,36 +55,38 @@ function drawBarVegaLite() {
                        .data(travel_data)
                        .transform(vl.filter(brush))
                        .encode(
-                         vl.y().fieldN('dist').axis({ labelAngle: 0}).sort(null).title('trip distance'),
-                         vl.x().average('value').title('The Number of Trips'),
-                         vl.color().value('grey').if(click, vl.color().fieldN('dist'))
+                         vl.y().fieldN('dist').axis({ labelAngle: 0, titleColor: '#63d0ff', labelColor: '#63d0ff', tickColor: '#63d0ff',domainColor: '#63d0ff'}).sort(null).title('trip distance'),
+                         vl.x().average('value').title('The Number of Trips').axis({ labelAngle: 0, grid: false, titleColor: '#63d0ff', labelColor: '#63d0ff', tickColor: '#63d0ff',domainColor: '#63d0ff'}),
+                         vl.color().if(click, vl.color().fieldN('dist').title('Distance Range')),
+                         vl.opacity().if(click, vl.value(1)).value(0.2)
                        )
-                       .title('Distribution of the Avg.Trips in United States in 2020')
-                       .width(1200)
+                       .title({text:'Distribution of the Avg.Trips in United States in 2020',color: '#63d0ff'})
+                       .width(1000)
     
     const trip_Point =vl.markPoint({filled: true})
                    .data(travel_data)
                    .encode(
-                     vl.x().month('Date'),
-                     vl.y().average('value').title('The Number of Trips'),
-                     vl.color().value('grey')
+                     vl.x().month('Date').axis({ labelAngle: 0, grid: false, titleColor: '#63d0ff', labelColor: '#63d0ff', domainColor: '#63d0ff'}),
+                     vl.y().average('value').title('The Number of Trips').axis({ labelAngle: 0, grid: false, titleColor: '#63d0ff', labelColor: '#63d0ff', tickColor: '#63d0ff', domainColor: '#63d0ff'}),
+                     vl.color().value('black')
                               .if(brush, vl.color().fieldN('dist')),
                      vl.opacity().if(click, vl.value(1)).value(0.2)
                    )
-                   .title('Trips Distance in Each Month')
+                   .title({text: 'Trips Distance in Each Month', color: '#63d0ff'})
                    .select(brush)
                    .transform(vl.filter(click))
-                   .width(1200)
+                   .width(1000)
     
     const trip_Line =vl.markLine()
                    .data(travel_data)
                    .encode(
-                     vl.x().month('Date'),
-                     vl.y().average('value').title('The Number of Trips'),
-                     vl.color().fieldN('dist'),
+                     vl.x().month('Date').axis({ labelAngle: 0, grid: false, titleColor: '#63d0ff', labelColor: '#63d0ff', tickColor: '#63d0ff', domainColor: '#63d0ff'}),
+                     vl.y().average('value').title('The Number of Trips').axis({ labelAngle: 0, grid: false, titleColor: '#63d0ff', labelColor: '#63d0ff', domainColor: '#63d0ff'}),
+                     vl.color().fieldN('dist').legend({fillColor:'black', titleColor: '#63d0ff', labelColor: '#63d0ff'}),
                      vl.opacity().if(click, vl.value(1)).value(0.2))
     
     return vl.vconcat( vl.layer(trip_Line, trip_Point), trip_2020)
+                .background({color: 'black'})
                 .autosize({type: 'fit-x', contains: 'padding'})
                 .resolve({scale: {y: 'shared'}})
                 .render()
